b59d08ca2ff7cb123e8b1a9a745ce40d1b702aad
Raghu K <raghuk.vit@gmail.com>
2025-04-28 06:18:41 +0530

    RoboShop Project

--- 03-variables/main.tf
+++ 03-variables/main.tf
@@ -6,3 +6,74 @@ output "x" {
   value = var.x
 }
 
+## Variable Data types
+# string
+# number
+# boolean
+# string require double quotes, number and boolean does not require quotes.
+
+variable "str" {
+  default = "Hello"
+}
+
+variable "num" {
+  default = 20
+}
+
+variable "mybool" {
+  default = true
+}
+
+# When we access the variable, irrespective of type you can access it with var.var-name
+
+# Variable requires to be in quotes if the variable is part of other characters. Also need to access that with ${var.var-nam}
+
+output "str" {
+  value = "${var.str}, Steve"
+}
+
+
+# Variable Types
+
+# Normal
+# List
+# Map / Dict
+
+variable "a" {
+  default = "THis is a normal Variable"
+}
+
+# The values in a list can comprise multiple data types
+variable "b" {
+  default = [
+    1,
+    2,
+    "abc",
+    false
+  ]
+}
+
+variable "c" {
+  default = {
+    x = 10
+    y = 20
+    z = "abc"
+  }
+}
+
+output "b1" {
+  value = var.b[0]
+}
+
+output "c1" {
+  value = var.c["x"]
+}
+
+
+
+
+
+
+
+
+
