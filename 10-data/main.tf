3370cb8bb807228b15753473d6bc4f5e8736f692
Raghu K <raghuk.vit@gmail.com>
2025-04-29 07:14:17 +0530

    RoboShop Project

--- 10-data/main.tf
+++ 10-data/main.tf
@@ -6,3 +6,8 @@ data "azurerm_resource_group" "example" {
 output "id" {
   value = data.azurerm_resource_group.example.id
 }
+
+provider "azurerm" {
+  features {}
+  subscription_id = "323379f3-3beb-4865-821e-0fff68e4d4ca"
+}
