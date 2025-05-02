651775fe2f453843843b2e5fd26fbac6a39b24e0
Raghu K <raghuk.vit@gmail.com>
2025-05-02 06:38:56 +0530

    RoboShop Project

---
+++ 12-hashi-vault/main.tf
@@ -0,0 +1,12 @@
+provider "vault" {
+  address = "http://vault.rdevopsb82.online:8200"
+  token = var.token
+}
+
+variable "token" {}
+
+data "vault_generic_secret" "secret" {
+  path = "demo/ssh"
+}
+
+
